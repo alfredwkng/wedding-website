3088ddb670c1f68afd6f41b8c8293bbbd52bc78a
alfredwkng <8890369+alfredwkng@users.noreply.github.com>
2026-05-30 12:15:00 -0400

    Updated design, JavaScript
    Updated fonts by changing font family in header
    
    Created Google Sheet to connect to RSVP form

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,19 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Annie and Alfred's Wedding Celebration",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('August 15, 2026, 6 p.m. to 10 p.m.'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
-
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            // duration: 240,
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '13 Greenpoint Avenue, Brooklyn, New York',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We look forward to celebrating our marriage with you."
         }
     });
 
@@ -218,7 +214,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyCOdFrBYq6CtogPv2N52dq8sEpdblzmZnpZdgqXoepoWkn0vA2swTzJTlwBAXHfZ9l3A/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -239,34 +235,6 @@ $(document).ready(function () {
 
 /********************** Extras **********************/
 
-// Google map
-function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: location,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-}
-
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: la_fiesta,
-        map: map
-    });
-}
 
 // alert_markup
 function alert_markup(alert_type, msg) {
